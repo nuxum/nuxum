f291305dd76f2c93e9f3f193f2659dde6611da36
ValDesign <valdesign.dev@gmail.com>
2024-10-20 15:25:38 +0200

    refactor: Update package.json version to 1.0.1

--- src/utils/app.interface.ts
+++ src/utils/app.interface.ts
@@ -1,5 +1,23 @@
 export type Class = new (...args: any[]) => any;
 
+type StaticOrigin = boolean | string | RegExp | Array<boolean | string | RegExp>;
+
+type CustomOrigin = (
+  requestOrigin: string | undefined,
+  callback: (err: Error | null, origin?: StaticOrigin) => void,
+) => void;
+
+interface CorsOptions {
+  origin?: StaticOrigin | CustomOrigin | undefined;
+  methods?: string | string[] | undefined;
+  allowedHeaders?: string | string[] | undefined;
+  exposedHeaders?: string | string[] | undefined;
+  credentials?: boolean | undefined;
+  maxAge?: number | undefined;
+  preflightContinue?: boolean | undefined;
+  optionsSuccessStatus?: number | undefined;
+}
+
 /**
  * AppOptions interface
  * @interface
@@ -25,7 +43,7 @@ export interface AppOptions {
    * cors: true
    * cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] }
    */
-  cors?: boolean | { origin?: string, methods?: string[] };
+  cors?: boolean | CorsOptions;
   /**
    * Default response headers
    * @default {}
